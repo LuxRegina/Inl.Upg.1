f5dfbca29e8b428912a6b3a733659419fe3439f0
LuxRegina <erika.s.hedstrom@gmail.com>
2023-11-03 10:22:27 +0100

    Added comments

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 {
     internal class Program
     {
-        static List<SweEngGloss> dictionary;
+        static List<SweEngGloss> dictionary;  // FixMe -   add new list
         class SweEngGloss
         {
             public string word_swe, word_eng;
@@ -16,26 +16,29 @@
                 this.word_swe = words[0]; this.word_eng = words[1];
             }
         }
+
+                                                                    // ToDo - Add Input method
+
         static void Main(string[] args)
         {
             string defaultFile = "..\\..\\..\\dict\\sweeng.lis";
-            Console.WriteLine("Welcome to the dictionary app!");
+            Console.WriteLine("Welcome to the dictionary app!");    // ToDo - add write help for commands and load which file to work with
             do
             {
                 Console.Write("> ");
-                string[] argument = Console.ReadLine().Split();
+                string[] argument = Console.ReadLine().Split();     
                 string command = argument[0];
-                if (command == "quit")
+                if (command == "quit")                              // ToDo - add 'Help' command
                 {
-                    Console.WriteLine("Goodbye!");
+                    Console.WriteLine("Goodbye!");                  
                 }
                 else if (command == "load")
                 {
                     if(argument.Length == 2)
                     {
-                        using (StreamReader sr = new StreamReader(argument[1]))
+                        using (StreamReader sr = new StreamReader(argument[1]))   // ToDo - sr - new name  and Catch if file isnt found
                         {
-                            dictionary = new List<SweEngGloss>(); // Empty it!
+                            dictionary = new List<SweEngGloss>(); // Prevents duplication if multiple loadouts are made!
                             string line = sr.ReadLine();
                             while (line != null)
                             {
@@ -47,9 +50,9 @@
                     }
                     else if(argument.Length == 1)
                     {
-                        using (StreamReader sr = new StreamReader(defaultFile))
+                        using (StreamReader sr = new StreamReader(defaultFile))     // ToDo - sr - new name  and Catch if file isnt found
                         {
-                            dictionary = new List<SweEngGloss>(); // Empty it!
+                            dictionary = new List<SweEngGloss>(); // Prevents duplication if multiple loadouts are made!
                             string line = sr.ReadLine();
                             while (line != null)
                             {
@@ -57,10 +60,10 @@
                                 dictionary.Add(gloss);
                                 line = sr.ReadLine();
                             }
-                        }
+                        } // Close file?
                     }
                 }
-                else if (command == "list")
+                else if (command == "list")                                         // NYI - Try-Catch if trying to list before loading a file
                 {
                     foreach(SweEngGloss gloss in dictionary)
                     {
@@ -71,14 +74,14 @@
                 {
                     if (argument.Length == 3)
                     {
-                        dictionary.Add(new SweEngGloss(argument[1], argument[2]));
+                        dictionary.Add(new SweEngGloss(argument[1], argument[2]));  // ToDo - CW state swedish word first
                     }
                     else if(argument.Length == 1)
                     {
                         Console.WriteLine("Write word in Swedish: ");
-                        string s = Console.ReadLine();
+                        string s = Console.ReadLine();                          // ToDo - new variable for s
                         Console.Write("Write word in English: ");
-                        string e = Console.ReadLine();
+                        string e = Console.ReadLine();                          // ToDo - new variable for e
                         dictionary.Add(new SweEngGloss(s, e));
                     }
                 }
@@ -92,14 +95,14 @@
                             if (gloss.word_swe == argument[1] && gloss.word_eng == argument[2])
                                 index = i;
                         }
-                        dictionary.RemoveAt(index);
+                        dictionary.RemoveAt(index);                            // NYI Catch if word requested isnt in the file
                     }
                     else if (argument.Length == 1)
                     {
                         Console.WriteLine("Write word in Swedish: ");
-                        string s = Console.ReadLine();
+                        string s = Console.ReadLine();                      // ToDo - new variable for s
                         Console.Write("Write word in English: ");
-                        string e = Console.ReadLine();
+                        string e = Console.ReadLine();                      // ToDo - new variable for e
                         int index = -1;
                         for (int i = 0; i < dictionary.Count; i++)
                         {
@@ -120,19 +123,19 @@
                                 Console.WriteLine($"English for {gloss.word_swe} is {gloss.word_eng}");
                             if (gloss.word_eng == argument[1])
                                 Console.WriteLine($"Swedish for {gloss.word_eng} is {gloss.word_swe}");
-                        }
+                        }                                                                                   // NYI - Catch if word(s) requested isnt in file
                     }
                     else if (argument.Length == 1)
                     {
                         Console.WriteLine("Write word to be translated: ");
-                        string s = Console.ReadLine();
+                        string s = Console.ReadLine();                          // ToDo - new variable for s
                         foreach (SweEngGloss gloss in dictionary)
                         {
                             if (gloss.word_swe == s)
                                 Console.WriteLine($"English for {gloss.word_swe} is {gloss.word_eng}");
                             if (gloss.word_eng == s)
                                 Console.WriteLine($"Swedish for {gloss.word_eng} is {gloss.word_swe}");
-                        }
+                        }                                                                                   // NYI - Catch if word(s) requested isnt in file
                     }
                 }
                 else
@@ -142,5 +145,6 @@
             }
             while (true);
         }
+                    // NYI - Add methods/functions for all arguments
     }
 }
